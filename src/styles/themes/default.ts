f01ed9efa2cbe673048a6d500f40b00a51513541
Rafael Luz <rafael@Nitro5-Rafael>
2024-04-22 11:10:52 -0300

    styles: cria stilização global e thema

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-100': '#e5e5e5',
+  'gray-300': '#b3b3b3',
+
+  'red-200': '#cf3333',
+  'red-400': '#c30000',
+
+  'blue-300': '#4da4bf',
+  'blue-900': '#012942',
+
+  'green-200': '#10e698',
+  'green-400': '#168C49',
+
+  black: '#0D0D0D',
+}
