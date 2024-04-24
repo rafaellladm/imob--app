f97e01192582c77ca24e8b9550fef67b1c9bb2c0
Rafael Luz <rafael@Nitro5-Rafael>
2024-04-24 00:10:04 -0300

    feat: estrutura a pagina de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,35 @@
+import imob from '../../assets/casa.jpeg'
+import logo from '../../assets/logo.svg'
+import {
+  BaseInput,
+  ContainerLeft,
+  ContainerRight,
+  EnterButton,
+  FarmContainer,
+  LoginContainer,
+} from './styles'
+
 export function Login() {
-  return <h1>Login</h1>
+  return (
+    <LoginContainer>
+      <ContainerLeft>
+        <img src={imob} alt="" />
+      </ContainerLeft>
+      <ContainerRight>
+        <img src={logo} alt="" />
+        <FarmContainer action="">
+          <label htmlFor="email">E-mail</label>
+          <BaseInput type="email" id="email" placeholder="exemplo@email.com" />
+          <label htmlFor="senha">Senha</label>
+          <BaseInput
+            type="password"
+            id="senha"
+            placeholder="Digite sua senha."
+          />
+          <EnterButton>Entrar</EnterButton>
+        </FarmContainer>
+        <a href="#">Esqueceu minha senha</a>
+      </ContainerRight>
+    </LoginContainer>
+  )
 }
